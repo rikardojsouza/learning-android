ca631f6f3d50486e477cfa19fb0bffae464df846
rikardojsouza <31066651+rikardojsouza@users.noreply.github.com>
2019-09-29 21:38:43 -0300

    Criando Alura Viagens
    Criando Projeto Alura Viagens

---
+++ AluraViagens/app/src/main/java/com/example/aluraviagens/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,16 @@
+package com.example.aluraviagens.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.aluraviagens.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
